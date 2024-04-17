11627e678fc1e858c837336733f2dd868ccaf3aa
yujin.lee <ywtv3636@naver.com>
2024-04-17 07:45:37 +0000

    incontext 생성

--- incontext.py
+++ incontext.py
@@ -1,3 +1,15 @@
 # incontext-learning을 위한 모듈. 기존에 누적된 내용을 학습하는 기능을 담당합니다.
 # 부연 설명이 필요할 듯합니다!
 # 
+
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+
+
+
+
+
+conversation = ConversationChain(llm=chat, memory=ConversationBufferMemory())
+
+
+conversation.run(enter())
