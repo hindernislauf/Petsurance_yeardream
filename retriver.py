14e3c0df648834012e892235f531176505786d14
ssukddeok <kwon3rd@naver.com>
2024-04-17 17:34:45 +0900

    Update retriver.py

--- retriver.py
+++ retriver.py
@@ -11,17 +11,9 @@ if "GOOGLE_API_KEY" not in os.environ:
     os.environ["GOOGLE_API_KEY"] = "AIzaSyDoXQh75wHDJV4S3is1WqniPzCjzqwPwSU"
 
 # web document를 가져오는 함수.
-loader = WebBaseLoader("https://docs.smith.langchain.com/overview")
+loader = WebBaseLoader("")
 text = loader.load() # raw text
 
-# 500글자씩 자르는데, 안겹치게 자름.
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=0)
-splits = text_splitter.split_documents(text)
-#print(len(splits))
-
-# split 해놓은 텍스트 데이터를 embedding한 VectorDB를 생성
-vectordb = Chroma.from_documents(documents=splits,
-                                 embedding=GoogleGenerativeAIEmbeddings(model="models/embedding-001"))
 
 # 사용자 입력(질문)과 비교해서 가까운 K개의 결과 찾기
 retriever = vectordb.as_retriever(k=1)
